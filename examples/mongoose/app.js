df83d344a9613e222ecc060d5541abb71eca9016
AlejoNext <alejonext@gmail.com>
2016-04-20 20:34:58 -0500

    babel

--- examples/mongoose/app.js
+++ examples/mongoose/app.js
@@ -1,16 +1,9 @@
 const express = require('express');
 var store = require('./store');
-
-var lang =require('../lib');
+var lang = require('../lib');
 var app = express();
 
-
-app.use(lang({
-	langs : 'en',
-	localte : 't',
-	store : new store('mongo://localhost:2017/myDB')
-}));
-
+app.use(new lang(store.getLang, store.listLang, store.opts).local);
 app.use(function(req, res){
 	res.json({
 		text : res.t(req.query.key),
